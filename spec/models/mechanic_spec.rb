70bca6b11ab4caeb90d1dc58684050f35fbac0d5
Sid Mann <sjmann2@outlook.com>
2022-09-12 10:52:18 -0500

    feat class method average years experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe Mechanic, type: :model do
 
   describe 'class methods' do
     describe 'average_years_experience' do
-      josh = Mechanic.create!(name: "Josh", years_experience: 5)
-      sandy = Mechanic.create!(name: "Sandy", years_experience: 10)
-      miya = Mechanic.create!(name: "Miya", years_experience: 2)
+      it 'calculates average years experience across all mechanics' do
+        josh = Mechanic.create!(name: "Josh", years_experience: 5)
+        sandy = Mechanic.create!(name: "Sandy", years_experience: 10)
+        miya = Mechanic.create!(name: "Miya", years_experience: 2)
 
-      expect(Mechanic.average_years_experience.round(2)).to eq(5.67)
+        expect(Mechanic.average_years_experience.round(2)).to eq(5.67)
+      end
     end
   end
 end
