5c818418efff744610201a6038e21cb4322bbbda
Sid Mann <sjmann2@outlook.com>
2022-09-12 11:32:42 -0500

    test/feat open? method

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Ride, type: :model do
     it { should have_many :ride_mechanics }
     it { should have_many(:mechanics).through(:ride_mechanics)}
   end
+
+  describe 'instance methods' do
+    describe 'open?' do
+      it 'returns true or false if a ride is open' do
+        six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+          
+        scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+        hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+        ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+        expect(scrambler.open?).to eq(true)
+        expect(hurler.open?).to eq(true)
+        expect(ferris.open?).to eq(false)
+      end
+    end
+  end
 end
