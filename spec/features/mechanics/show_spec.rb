71416cffe58ee9e9e9e2405390b6ad4eed7cf527
Sid Mann <sjmann2@outlook.com>
2022-09-12 11:21:24 -0500

    test rides listed in order by most thrills

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe 'the mechanic show page' do
           it 'lists the mechanic name, years experience, and rides theyre working on' do
             six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
           
-            hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
             scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+            hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
             ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
             josh = Mechanic.create!(name: "Josh", years_experience: 5)
@@ -24,14 +24,38 @@ RSpec.describe 'the mechanic show page' do
             josh_scrambler = RideMechanic.create!(ride_id: scrambler.id, mechanic_id: josh.id)
             
             visit "/mechanics/#{josh.id}"
-            save_and_open_page
+            
             expect(page).to have_content(josh.name)
             expect(page).to have_content(josh.years_experience)
             expect(page).to have_content(hurler.name)
             expect(page).to have_content(scrambler.name)
           end
 
-          it 'lists rides that are open ordered by thrill in descending order'
+          it 'lists rides that are open ordered by thrill in descending order' do
+            six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+          
+            hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+            scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+            wild_thing = six_flags.rides.create!(name: 'The Wild Thing', thrill_rating: 8, open: true)
+            ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+            josh = Mechanic.create!(name: "Josh", years_experience: 5)
+
+            josh_hurler = RideMechanic.create!(ride_id: hurler.id, mechanic_id: josh.id)
+            josh_scrambler = RideMechanic.create!(ride_id: scrambler.id, mechanic_id: josh.id)
+            josh_ferris = RideMechanic.create!(ride_id: ferris.id, mechanic_id: josh.id)
+            josh_wild_thing = RideMechanic.create!(ride_id: wild_thing.id, mechanic_id: josh.id)
+
+            visit "/mechanics/#{josh.id}"
+
+            expect(page).to have_content(hurler.name)
+            expect(page).to have_content(wild_thing.name)
+            expect(page).to have_content(scrambler.name)
+            expect(page).to_not have_content(ferris.name)
+
+            expect(wild_thing.name).to appear_before(hurler.name)
+            expect(hurler.name).to appear_before(scrambler.name)
+          end
         end
       end
     end
