31522a0bb0c4586162d167a8e0ca8fd303311f60
Sid Mann <sjmann2@outlook.com>
2022-09-12 10:49:13 -0500

    test average years experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :ride_mechanics }
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe 'class methods' do
+    describe 'average_years_experience' do
+      josh = Mechanic.create!(name: "Josh", years_experience: 5)
+      sandy = Mechanic.create!(name: "Sandy", years_experience: 10)
+      miya = Mechanic.create!(name: "Miya", years_experience: 2)
+
+      expect(Mechanic.average_years_experience.round(2)).to eq(5.67)
+    end
+  end
 end
